2bf889de73ae16c4755ba920514025bee6726715
adam <adaml@gini-apps.com>
2021-09-14 17:35:30 +0300

    done with episode 23

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
-import React, {useCallback, useState, useEffect, useReducer, useRef} from 'react';
+import React, {useCallback, useRef} from 'react';
 import './App.css';
+import {useTodos} from './useTodos' // this is our custom hook - woohoo!
 
 
 const Heading = ({title}: { title: string }) => <h2>{title}</h2>
@@ -14,45 +15,16 @@ const Box: React.FunctionComponent = ({children}) =>
   </div>
 )
 
-interface Todo {
-  id: number;
-  text: string;
-}
-
-type ActionType =
-  | { type: "ADD", text: string }
-  | { type: "REMOVE", id: number }
-
 function App() {
-
-  const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
-    switch (action.type) {
-      case "ADD":
-        return [
-          ...state, // this would be the array of existing todos
-          {
-            id: state.length,
-            text: action.text,
-          },
-        ];
-      case "REMOVE":
-        return state.filter(({id}) => id !== action.id);
-      default:
-        throw new Error()
-    }
-  }, [])
-
+  const {todos, addTodo, removeTodo} = useTodos([{id:0, text:"hey there"}])
   const newTodoRef = useRef<HTMLInputElement>(null)
 
   const onAddTodo = useCallback(() => {
-    if (newTodoRef.current) { // typescript is smart enough to know that here we are checking this value is not undefined/null...
-      dispatch({
-        type: "ADD",
-        text: newTodoRef.current.value // ... so that we dont get an error here, telling us it might be undefined/null
-      })
+    if (newTodoRef.current) {
+      addTodo(newTodoRef.current.value)
       newTodoRef.current.value = "";
     }
-  }, [])
+  }, [addTodo])
 
   const Button: React.FunctionComponent<
     React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
@@ -75,10 +47,7 @@ function App() {
       {todos.map(todo => (
         <div key={todo.id}>
           {todo.text + ' '}
-          <button onClick={() => dispatch({
-            type: "REMOVE",
-            id: todo.id,
-          })}>Remove
+          <button onClick={() =>removeTodo(todo.id)}>Remove
           </button>
         </div>
       ))}
